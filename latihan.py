a4768a291a9d97054a1a3ac97500190943e04c8b
Ramdani <xhijack@gmail.com>
2023-07-18 16:17:42 +0700

    inisial

---
+++ latihan.py
@@ -0,0 +1,8 @@
+print("Masukan Satuan Yard:")
+yard = float(input())
+print("Masukan Roll")
+roll = int(input())
+total = yard * roll
+print("Masukan Total Uang nya")
+uang = float(input())
+print("total nya adalah ",total,"yard", " dan setiap yardnya adalah", uang/total)
