6a7bb0f3daa4884ecfb4da4c16950ce8875c189b
naufalfdlnr <naufalfdln89@gmail.com>
2023-07-21 14:12:25 +0700

    pindah di folder latihan

--- NAUFAL/1.py
+++
@@ -1,9 +0,0 @@
-x = int(input('nilai harta : '))
-y = int(input('nilai emas : '))
-if x > y*85:
-    z = input('sudah masuk haul YA/TIDAK: ')
-    if x > y*85 and z == 'YA':
-        x *= 0.025
-        print('zakat yang harus di bayar : ', x)
-else:
-    print('harta belum bisa di zakatkan')
--- NAUFAL/2.py
+++
@@ -1,9 +0,0 @@
-x = int(input('masukan angka: '))
-y = int(input('masukan angka: '))
-z = int(input('masukan angka: '))
-if x == y and x == z and y == z:
-    print('segitiga sama sisi')
-elif x == y or x == z or y == z:
-    print('segitiga sama kaki')
-else:
-    print('segitiga tak beraturan')
